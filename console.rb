3256406598476610a90bcaea0983d418f8449c9f
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-01 14:40:28 +0100

    changed to just name for artist

---
+++ console.rb
@@ -0,0 +1,15 @@
+require('pry-byebug')
+require_relative('models/album.rb')
+require_relative('models/artist.rb')
+
+artist1 = Artist.new({
+    'name' => 
+})
+
+album1 = Album.new({
+
+
+})
+
+binding.pry
+nil
--- models/artist.rb
+++ models/artist.rb
@@ -3,12 +3,11 @@ require_relative('../db/sql_runner')
 
 class Artist
 
-    attr_reader :id, :first_name, :last_name
+    attr_reader :id, :name
 
     def initialize(options)
         @id = options['id'] if options['id']
-        @first_name = otions['first_name']
-        @last_name = options['last_name']
+        @name = options['name']
     end
 
 
