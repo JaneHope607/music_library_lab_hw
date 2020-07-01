914f9c947037536c1ebd00889c1bc69c1a0e7691
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-01 14:45:09 +0100

    created new objects - artist1 and album1

--- console.rb
+++ console.rb
@@ -3,12 +3,13 @@ require_relative('models/album.rb')
 require_relative('models/artist.rb')
 
 artist1 = Artist.new({
-    'name' => 
+    'name' => 'The Black Keys'
 })
 
 album1 = Album.new({
-
-
+    'title' => 'El Camino'
+    'genre' => 'rock'
+    'artist_id' => artist1.id
 })
 
 binding.pry
